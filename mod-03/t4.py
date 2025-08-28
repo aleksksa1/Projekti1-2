7608a028c336b00a230393005045a6e781089070
aleksksa1 <aleksi.sakkinen@metropolia.fi>
2025-08-28 10:09:58 +0300

    päivitetty

--- mod-03/t4.py
+++ mod-03/t4.py
@@ -1,6 +1,6 @@
 vuosi = float(input('Syötä vuosiluku: '))
 
-if (vuosi % 4 == 0 and not vuosi % 100 == 0) or vuosi % 400 == 0:
+if (vuosi % 4 == 0 and not vuosi % 100 == 0)  or vuosi % 400 == 0:
     print('On karkausvuosi')
 
 else: print('Ei ole karkausvuosi')
